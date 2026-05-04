86808ad9d93d3b682c94cd69794f1e7e9058a1cb
Anik Baul <anikbaul2@gmail.com>
2026-05-04 22:42:40 +0530

    feature-user-navigator

--- cipher-quiz/src/App.jsx
+++ cipher-quiz/src/App.jsx
@@ -3,6 +3,7 @@ import QuestionCard from "./components/User/QuestionCard";
 import Subheading from "./components/User/Subheading";
 import "./App.css";
 import QuestionForm from "./components/Creator/QuestionForm";
+import Reflector from "./components/User/Reflector";
 
 const App = () => {
   const [questions, setQuestions] = useState([]);
@@ -10,17 +11,51 @@ const App = () => {
   const handleSave = (q) => {
     setQuestions([...questions, q]);
   };
+  const questionSet = [
+    {
+      id: 1,
+      question: "1+1",
+      type: "mcq",
+      options: [1, 2, 3, 4],
+    },
+    {
+      id: 2,
+      question: "1+2",
+      type: "mcq",
+      options: [1, 2, 3, 4],
+    },
+    {
+      id: 2,
+      question: "1+2",
+      type: "text",
+    },
+  ];
   return (
     <>
       <h1 className="main-heading">Cipher Quiz</h1>
       <Subheading desc="Basic Quiz" author="HA" tmarks="50" ttime="60 mins" />
-      <QuestionCard
-        question="1+1= .?"
-        options={["1", "2", "3", "4"]}
-        onSelect={(ans) => console.log("Selected:", ans)}
-        type="mcq"
-      />
-      <QuestionForm onSave={handleSave} />
+
+      <div className="main-container">
+        <div className="qs-form-container">
+          {questionSet.map((qs) => (
+            <QuestionCard
+              key={qs.id}
+              type={qs.type}
+              question={qs.question}
+              options={qs.options}
+              onSelect={(ans) => console.log("Selected:", ans)}
+            />
+          ))}
+          <QuestionForm onSave={handleSave} />
+        </div>
+        <div className="side-show">
+          <Reflector
+            totalQs={40}
+            attempted={[true,true,false,false,false,false,false,true,true,true,true,false,true,false,true]}
+            timeLeft={150}
+          />
+        </div>
+      </div>
     </>
   );
 };
---
+++ cipher-quiz/src/components/User/Reflector.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../../StyleSheet/Reflector.css";
+
+const Reflector = ({ totalQs, attempted = [], timeLeft }) => {
+  const formatTime = (seconds) => {
+    const m = String(Math.floor(seconds / 60)).padStart(2, "0");
+    const s = String(seconds % 60).padStart(2, "0");
+    return `${m}:${s}`;
+  };
+
+  return (
+    <div className="reflector">
+
+      <div className="time">
+        {formatTime(timeLeft)}
+      </div>
+
+      <div className="options">
+        {Array.from({ length: totalQs }).map((_, i) => (
+          <div
+            key={i}
+            className={`q-box ${
+              attempted[i] ? "attempted" : "not-attempted"
+            }`}
+          >
+            {i + 1}
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default Reflector;
