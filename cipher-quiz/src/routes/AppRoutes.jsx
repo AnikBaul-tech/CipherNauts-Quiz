91fbb22c931b84d3941584c874bb4b4700f59074
Anik Baul <anikbaul2@gmail.com>
2026-07-06 14:17:38 +0530

    new routes

--- cipher-quiz/src/routes/AppRoutes.jsx
+++ cipher-quiz/src/routes/AppRoutes.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AuthPage from '../pages/Auth';
-import QuizForm from '../pages/QuizForm';
-import QuestionForm from '../components/Creator/QuestionForm';
-import QuestionMakingForm from '../pages/QuestionMakingForm';
-import Home from '../pages/Home';
-import ProtectRoute from './ProtectRoute';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuthPage from "../pages/Auth";
+import QuizForm from "../pages/QuizForm";
+import QuestionForm from "../components/Creator/QuestionForm";
+import QuestionMakingForm from "../pages/QuestionMakingForm";
+import RequestRegister from "../pages/RequestRegister";
+import Home from "../pages/Home";
+import ProtectRoute from "./ProtectRoute";
+import QuizRoom from "../pages/QuizRoom";
+import Quiz from "../pages/Quiz";
+import Profile from "../pages/Profile";
+import Result from "../pages/Result";
 
 const AppRoutes = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route index element={<AuthPage />} />
-                <Route element={<ProtectRoute />}>
-                    <Route path="/quiz-form" element={<QuizForm />} />
-                    <Route path="/quiz-making-form" element={<QuestionMakingForm />} />
-                    <Route
-                        path="/quiz-making-form/:quizId"
-                        element={<QuestionForm />}
-                    />
-                    <Route path="/home" element={<Home />} />
-                </Route>
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<AuthPage />} />
+        <Route element={<ProtectRoute />}>
+          <Route path="/quiz-form" element={<QuizForm />} />
+          <Route path="/quiz-making-form" element={<QuestionMakingForm />} />
+          <Route path="/quiz-room/:quizId" element={<QuizRoom />} />
+          <Route path="/quiz-making-form/:quizId" element={<QuestionForm />} />
+          <Route path="quiz-request" element={<RequestRegister />}/>
+          <Route path="/attempt-quiz/:quizId" element={<Quiz />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+          <Route path="/result/:quizId/:userId" element={<Result />} />
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
